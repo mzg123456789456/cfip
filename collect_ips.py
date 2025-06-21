c520a4d145ef570f5744aa2e5f0778e395890df9
mzg123456789456 <116986180+mzg123456789456@users.noreply.github.com>
2025-06-21 20:04:21 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -15,30 +15,35 @@ ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-# 创建一个文件来存储IP地址
-with open('ip.txt', 'w') as file:
-    for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(url)
-        
-        # 使用BeautifulSoup解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://api.uouin.com/cloudflare.html':
-            elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
-            elements = soup.find_all('tr')
-        else:
-            elements = soup.find_all('li')
-        
-        # 遍历所有元素,查找IP地址
-        for element in elements:
-            element_text = element.get_text()
-            ip_matches = re.findall(ip_pattern, element_text)
-            
-            # 如果找到IP地址,则写入文件
-            for ip in ip_matches:
-                file.write(ip + '\n')
+# 使用集合来存储IP地址，自动去重
+unique_ips = set()
 
-print('IP地址已保存到ip.txt文件中。')
+for url in urls:
+    # 发送HTTP请求获取网页内容
+    response = requests.get(url)
+    
+    # 使用BeautifulSoup解析HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # 根据网站的不同结构找到包含IP地址的元素
+    if url == 'https://api.uouin.com/cloudflare.html':
+        elements = soup.find_all('tr')
+    elif url == 'https://ip.164746.xyz':
+        elements = soup.find_all('tr')
+    else:
+        elements = soup.find_all('li')
+    
+    # 遍历所有元素,查找IP地址
+    for element in elements:
+        element_text = element.get_text()
+        ip_matches = re.findall(ip_pattern, element_text)
+        
+        # 将找到的IP地址添加到集合中（自动去重）
+        unique_ips.update(ip_matches)
+
+# 将去重后的IP地址写入文件
+with open('ip.txt', 'w') as file:
+    for ip in unique_ips:
+        file.write(ip + '\n')
+
+print(f'共找到 {len(unique_ips)} 个唯一IP地址，已保存到ip.txt文件中。')
